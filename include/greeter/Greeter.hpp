71199e8c5d9556b9930c2faacec087b309017ec8
Lars Ivar Hatledal <laht@ntnu.no>
2023-02-13 12:25:01 +0100

    header-only

--- include/greeter/Greeter.hpp
+++ include/greeter/Greeter.hpp
@@ -3,13 +3,16 @@
 #define GREETER_GREETER_HPP
 
 #include <string>
+#include <iostream>
 
 class Greeter {
 
 public:
     explicit Greeter(const std::string &name) : name_(name) {}
 
-    void greet();
+    void greet() const {
+        std::cout << "Hello " << name_ << "!" << std::endl;
+    }
 
 private:
     std::string name_;
--- src/greeter/Greeter.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "greeter/Greeter.hpp"
-
-#include <iostream>
-
-void Greeter::greet() {
-    std::cout << "Hello " << name_ << "!" << std::endl;
-}
